7f1e0dcb9c29588b99fb919c51cbd52dd9f2da56
fguintu <frederic.guintu@emory.edu>
2024-04-07 22:41:24 -0400

    added resume_compiler.py

---
+++ src/resume_compiler.py
@@ -0,0 +1,16 @@
+import subprocess
+
+
+def compile_latex(input_file, compiler="lualatex"):
+    try:
+        # Run the LuaLaTeX compiler
+        subprocess.run([compiler, input_file], check=True)
+        print(f"Compilation successful: {input_file} has been compiled to PDF.")
+    except subprocess.CalledProcessError:
+        print(f"Error: Compilation failed for {input_file}.")
+
+
+# Replace 'document.tex' with your LaTeX file name
+if __name__ == '__main__':
+    latex_file = '../dat/resume_template/bba_resume_template.tex'
+    compile_latex(latex_file)
