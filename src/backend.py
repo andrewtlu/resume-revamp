2dc3347d087f1f9416ff3721483c17437ac10148
Andrew Chung <achun46@emory.edu>
2024-04-20 23:11:14 -0400

    small changes

--- src/backend.py
+++ src/backend.py
@@ -20,13 +20,27 @@ def home():
 def handle_initial_prompt():
     data = request.get_json()
 
-    client = anthropic.Anthropic()  # Replace ... with your initialization parameters
+    client = anthropic.Anthropic() 
 
     resume = data.get('resume', {})
     key = data.get('key', '')
     response = pr.initial_prompt(client, resume, key)
 
-    # Return the response as JSON
+    return jsonify({'result': response})
+
+
+@app.route('/sub_prompt', methods=['POST'])
+def handle_sub_prompt():
+    data = request.get_json()
+
+    client = anthropic.Anthropic() 
+
+
+
+    resume = data.get('resume', {})
+    key = data.get('key', '')
+    response = pr.sub_prompts(client, resume, key)
+
     return jsonify({'result': response})
 
 
@@ -46,13 +60,19 @@ def parse_resume_endpoint():
 
     parsed_resume = parse_resume(client, pdf_path)
 
-    json_path = os.path.join(uploads_dir, 'resume_parsed.json')
-    with open(json_path, 'w') as f:
-        json.dump(parsed_resume, f, indent=2)
+    if parsed_resume is not None:
+        json_path = os.path.join(uploads_dir, 'resume_parsed.json')
+        
+        with open(json_path, 'w') as f:
+            json.dump(parsed_resume, f, indent=2)
+        
+        os.remove(pdf_path)
 
-    os.remove(pdf_path)
-
-    return jsonify({'message': 'Resume parsed successfully'}), 200
+        return jsonify({'message': 'Resume parsed successfully'}), 200
+    else:
+        #TODO: new prompt for user to add the missing information
+        # new prompt for user to add the missing information
+        pass
 
 
 
